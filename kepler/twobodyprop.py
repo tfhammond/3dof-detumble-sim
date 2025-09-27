4e1ed4f326bf617a102f74d757434d3e6508bd24
Thomas <59429963+tfhammond@users.noreply.github.com>
2025-09-26 22:58:41 -0700

    Add RK4 integration to TwoBodyPropagator class
    Implemented equation_of_motion and rk4_step methods in TwoBodyPropagator for numerical integration. Added imports for KeplerElements. Also created empty attitude/rotation.py and math/math.py modules for future development.

--- kepler/twobodyprop.py
+++ kepler/twobodyprop.py
@@ -1,5 +1,21 @@
 import numpy as np
+from tle_io.keplerelement import KeplerElements
 
 
 class TwoBodyPropagator:
+    def equation_of_motion(self, state):
+        r =state[:3]
+        v = state[3:]
+        r_norm = np.linalg.norm(r)
+        a = -self.mu * r / r_norm**3
+        return np.hstack((v, a))
     
+    def rk4_step(self, state, dt):
+        k1 = self.equation_of_motion(state)
+        k2 = self.equation_of_motion(state + 0.5 * dt * k1)
+        k3 = self.equation_of_motion(state + 0.5 * dt * k2)
+        k4 = self.equation_of_motion(state + dt * k3)
+        return state + (dt / 6) * (k1 + 2*k2 + 2*k3 + k4)
+
+
+
