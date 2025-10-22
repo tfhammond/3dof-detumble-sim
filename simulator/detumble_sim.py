fixed quaternion sign bug

simulator/detumble_sim.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from controller.bdotcontroller import BDotController
 from attitude_dynamics.dynamics import Dynamics
-from magnetic_field.model import MagneticFieldModel, rotate_eci, GMSTTracker, gmst_angle_rad
+from magnetic_field.model import MagneticFieldModel, rotate_eci, GMSTTracker, gmst_angle_rad, eci_to_body
 
 from simulator.config import DetumbleConfig
 
@@ -94,7 +94,8 @@ class DetumbleSim:
             #test
             field_time += perf_counter() - t1
 
-            b_body_T = rotate_eci(self.att.q_IB, b_eci_T) #???
+            #b_body_T = rotate_eci(self.att.q_IB, b_eci_T) #???
+            b_body_T = eci_to_body(self.att.q_IB, b_eci_T)
             b_norm = float(np.linalg.norm(b_body_T))
             #TEST
             t_cmd = perf_counter()
@@ -133,7 +134,8 @@ class DetumbleSim:
             elapsed = 0.0
             for i in range(Nsub):
                 t1 = perf_counter()
-                b_body_sub_T = rotate_eci(self.att.q_IB, b_eci_T)
+                #b_body_sub_T = rotate_eci(self.att.q_IB, b_eci_T)
+                b_body_sub_T = eci_to_body(self.att.q_IB, b_eci_T)
                 field_time += perf_counter() - t1
                 tau_c_B = np.cross(m_eff, b_body_sub_T)
                 #test

simulator/sim.py
@@ -64,7 +64,7 @@ def build_sim():
     h = 0.01
     duty = 0.6
     m_bar = np.array([0.002, 0.002, 0.002]) # A*m^2
-    polarity = np.array([-1, -1, -1], dtype=int)
+    polarity = np.array([1, 1, 1], dtype=int)
     I_min = float(np.min(np.diag(I)))
     omega_orbit = float(kep.n)
     xi_geomag = kep.i
@@ -296,7 +296,8 @@ def run():#
 
     sim, x_orbit0 = build_sim()
 
-    sim_duration = 18.5 * 1.53 * 3600.0 # n orbits?
+    #sim_duration = 18.5 * 1.53 * 3600.0 # n orbits?
+    sim_duration = 3000
 
     t_final = t0 + timedelta(seconds=sim_duration)
  